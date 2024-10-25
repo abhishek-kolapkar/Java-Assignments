7139af2d1cc3acb9d9ec43d6057520d3e33dcffc
abhishek-kolapkar <abhisheknkolapkar@gmail.com>
2024-10-25 10:53:27 +0530

    Task-1 solved

---
+++ Inheritance_Polymarphism_Encapsulation/Inheritance.java
@@ -0,0 +1,25 @@
+class Animal {
+  String species;
+  int age;
+
+  // default constructor
+  public Animal() {
+    this.species = "Bulldog";
+    this.age = 6;
+  }
+
+  // method to print animal details
+  public void displayInfo() {
+    System.out.println("Species: " + species + "\nAge: " + age);
+  }
+}
+
+class Dog extends Animal {
+  public static void main(String[] args) {
+    // instance of Dog class
+    Dog dog = new Dog();
+
+    // access displayInfo from Animal class
+    dog.displayInfo();
+  }
+}
