4b66a883b0e02bd82623e6ede2ea3c775ad9fee7
abhishek-kolapkar <abhisheknkolapkar@gmail.com>
2024-11-13 22:31:42 +0530

    Task-1 solved

---
+++ Checked_Unchecked_Exception/DivisionByZero.java
@@ -0,0 +1,19 @@
+public class DivisionByZero {
+  public static int divide(int a, int b) {
+    try {
+      return a / b;
+    } catch (Exception e) {
+      System.out.println("Cannot divide by zero");
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    int result1 = divide(12, 2);
+    System.out.println("Result of 12/2: " + result1);
+
+    int result2 = divide(12, 0);
+    System.out.println("Result of 12/0: " + result2);
+  }
+}
+
